fc63aa4951d3598daafd035b888919a04515a8ed
Ashish <salv0045@flinder.edu.au>
2021-08-02 00:53:21 +0930

    commit

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This is a portfolio website built using ReactJs
 
-Link: https://sanket-salunke.netlify.app/
+Link: ashish-salve-b0640b.netlify.app
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,8 +19,7 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              {/* <li>&copy; Copyright 2017 Tim Baker</li> */}
-              {/* <li>Sanket Salunke <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li> */}
+              
            </ul>
 
         </div>
